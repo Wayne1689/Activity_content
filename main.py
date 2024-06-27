9cb2d1188d52d5fb9488b09b0e6a94aa1ac3df80
Wayne1689 <138987813+Wayne1689@users.noreply.github.com>
2024-06-28 00:45:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,14 +10,21 @@ opt = Options()
 opt.add_argument('--ignore-certificate-errors')
 driver = webdriver.Chrome(options=opt)
 driver.get('https://sys.ndhu.edu.tw/SA/XSL_ApplyRWD/ActApply.aspx')
-driver.find_element(By.ID,'BodyContent_gvActs_lbtGridShow_0').click()
 
-#抓取文檔
-text_element = driver.find_element(By.ID,'BodyContent_txt_act_content')
-text_content = text_element.text
 
-#用來將文檔寫入指定路徑
-file_path = r"C:/Users/s3919/OneDrive/桌面/爬取的文檔/Web_content.txt"
+for i in range(0,4):
 
-with open(file_path, "w", encoding="utf-8") as file:
-    file.write(text_content)
+    #點擊
+    driver.find_element(By.ID,f'BodyContent_gvActs_lbtGridShow_{i}').click()
+    
+    #抓取文檔
+    text_element = driver.find_element(By.ID,'BodyContent_txt_act_content')
+    text_content = text_element.text
+    
+    #返回
+    driver.find_element(By.NAME,'ctl00BodyContentBodyContentbtnBackFromModifyAct').click()
+
+    #用來將文檔寫入指定路徑
+    file_path = f"C:/Users/s3919/OneDrive/桌面/爬取的文檔/Web_content{i}.txt"
+    with open(file_path, "w", encoding="utf-8") as file:
+        file.write(text_content)
